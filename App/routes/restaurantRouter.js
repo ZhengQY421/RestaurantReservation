0a5387fd7771e1c65005b2d600f05aa21aff9cf6
adriannafu <afu4599@gmail.com>
2019-04-08 20:21:08 +0800

    Revert "Update gitignore"
    This reverts commit 996c969949964b47412d76b260f6dc9a905fe9a4.

--- App/routes/restaurantRouter.js
+++ App/routes/restaurantRouter.js
@@ -4,15 +4,15 @@ var router = express.Router();
 /* Connect to Database */
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+	connectionString: process.env.DATABASE_URL
 });
 
 /* ---- GET for show all restaurants ---- */
 router.get('/', function(req, res, next) {
 	pool.query('SELECT * FROM Restaurants', (err, data) => {
         console.log(err)
-		res.render('restaurant/restaurant', {
-			title: 'All Restaurants',
+		res.render('restaurant/restaurant', { 
+			title: 'All Restaurants', 
 			data: data.rows,
 			currentUser: req.user});
 	});
@@ -24,10 +24,10 @@ router.get('/search', function(req,res,next) {
 		console.log(err)
 		console.log(req.query)
 		res.render('restaurant/search', {
-			title: 'Search Restaurants',
+			title: 'Search Restaurants', 
 			data: data.rows,
 			currentUser: req.user});
 	});
-  });
+  }); 
 
 module.exports = router;
