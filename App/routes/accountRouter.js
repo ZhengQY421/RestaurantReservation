2fabc801b9f68bcd022e8d6ca2826a5d550a6220
edenuis <edenuis@gmail.com>
2019-04-12 16:56:25 +0800

    Updates

--- App/routes/accountRouter.js
+++ App/routes/accountRouter.js
@@ -1,17 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var passport = require('passport');
+var passport = require("passport");
 
 /* Connect to Database */
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
 });
 
-const {
-    checkLoggedIn,
-    checkLoggedOut,
-} = require("./middleware/auth");
+const { checkLoggedIn, checkLoggedOut } = require("./middleware/auth");
 
 function failRegister(req, res) {
     req.flash("error", "Sorry, an error has occurred. Please try again later.");
@@ -19,123 +16,207 @@ function failRegister(req, res) {
 }
 
 function makeid(name) {
-
     var loc = name.indexOf(" ");
-    var first = name.slice(0,loc).toLowerCase();
-    var last = name.slice(loc+1).toLowerCase();
+    var first = name.slice(0, loc).toLowerCase();
+    var last = name.slice(loc + 1).toLowerCase();
     last.replace(" ", "");
     var digit = Math.floor(Math.random() * 11);
 
-    return first.slice(0,1)+last+digit+"";
+    return first.slice(0, 1) + last + digit + "";
 }
 
 /* ---- Post Function for Login ---- */
-router.post('/login', checkLoggedOut,
-passport.authenticate("local", {
-    failureRedirect: "/",
-    failureFlash: true
-  }),
-function(req,res,next){
-    req.flash("success", "You have logged in!");
-    res.redirect("/");
-
-});
+router.post(
+    "/login",
+    checkLoggedOut,
+    passport.authenticate("local", {
+        failureRedirect: "/",
+        failureFlash: true
+    }),
+    function(req, res, next) {
+        req.flash("success", "You have logged in!");
+        res.redirect("/");
+    }
+);
 
 /* ---- Get/Post Function for Log Out ---- */
-router.all('/logout', checkLoggedIn, function(req,res,next){
+router.all("/logout", checkLoggedIn, function(req, res, next) {
     req.logout();
     req.flash("success", "You have been logged out!");
     res.redirect("/");
 });
 
 /* ---- Post Function for Sign up ---- */
-router.post('/signup', checkLoggedOut, function(req, res, next){
-
+router.post("/signup", checkLoggedOut, function(req, res, next) {
     pool.query(
-        "select 1 from users where email=$1;",[req.body.signupEmail],
-        function(err,data){
-            if(err){
+        "select 1 from users where email=$1;",
+        [req.body.signupEmail],
+        function(err, data) {
+            if (err) {
                 console.log(err);
                 failRegister(req, res);
-            } else{
-
-                if (data.rowCount === 0){
-
+            } else {
+                if (data.rowCount === 0) {
                     var name = req.body.signupName;
                     var email = req.body.signupEmail;
                     var password = req.body.signupPassword;
-                    var uid = makeid(name);
                     var addr = req.body.signupAddr;
                     var pNum = req.body.signupPNum;
 
-                    pool.query("insert into Users (uid, name, email, password) values ($1, $2, $3, $4)",
-                    [uid,name,email,password],
-                    (err,data) => {
-                        if (err) {
-                            console.error('Error executing query', err.stack);
+                    pool.query(
+                        "insert into Users (name, email, password) values ($1, $2, $3)",
+                        [name, email, password],
+                        function(err, data) {
+                            if (err) {
+                                console.error(
+                                    "Error executing query",
+                                    err.stack
+                                );
+                            } else {
+                                var sql_query = "";
+
+                                if (req.body.signupType === "Customer") {
+                                    console.log(addr);
+                                    sql_query =
+                                        "INSERT INTO Customers(uid, address, pNumber, rewardPt) values ((select U.uid from Users U where U.name=" +
+                                        "'" +
+                                        name +
+                                        "'),'" +
+                                        addr +
+                                        "','" +
+                                        pNum +
+                                        "', 0)";
+                                } else if (req.body.signupType === "Owner") {
+                                    req.session.valid = name;
+                                    res.redirect(303, "/restaurant/add");
+                                    return;
+                                }
+
+                                pool.query(sql_query, (err, data) => {
+                                    if (err) {
+                                        console.log(err);
+                                        console.log("error insert");
+                                        failRegister(req, res);
+                                        return;
+                                    }
+                                    req.flash(
+                                        "success",
+                                        "Account created. You may log in now."
+                                    );
+                                    res.redirect("/");
+                                });
+                            }
                         }
-                    });
-
-                    var sql_query = "";
-
-                    if (req.body.signupType === "Customer"){
-                        console.log(addr);
-                        sql_query = "insert into Customers (uid, address, pNumber, rewardPt) values" + "('" + uid + "','" + addr + "','" + pNum + "',0" + ")";
-
-                    }else if (req.body.signupType === "Owner"){
-                        sql_query = "insert into Owners (uid, bid) values" + "('" + uid + "',null" + ")";
-                    }
-
-                    pool.query(sql_query, (err,data) => {
-                        if (err){
-                            console.log(err);
-                            console.log("error insert");
-                            failRegister(req,res);
-                            return;
-                        }
-                        req.flash("success", "Account created. You may log in now.");
-                        res.redirect("/");
-                    });
-
-                }else {
-                    req.flash("warning", "Account already exists, please login.")
-                    res.redirect("/")
+                    );
+                } else {
+                    req.flash(
+                        "warning",
+                        "Account already exists, please login."
+                    );
+                    res.redirect("/");
                 }
             }
-
         }
-    )
+    );
+});
+
+router.post("/addOwner", checkLoggedOut, function(req, res, next) {
+    var info = req.session.valid;
+    req.session.valid = null;
+
+    pool.query(
+        "insert into owners(uid, rid, bid) values ((select U.uid from Users U where U.name=$1), (select R.rid from restaurants R where R.name=$2), (select bid from branches b where b.rid=(select R.rid from restaurants R where R.name=$2)))",
+        [info[1], info[0]],
+        (err, data) => {
+            console.log(req.body.name);
+            if (err) {
+                console.log(err);
+                console.log("error inserting owner");
+                failRegister(req, res);
+                return;
+            }
+            req.flash(
+                "success",
+                "Account and restaurant created. You may log in now."
+            );
+            res.redirect("/");
+        }
+    );
 });
 
 /* ---- GET for profile ---- */
-router.get('/profile', checkLoggedIn, function(req, res, next){
-
+router.get("/profile", checkLoggedIn, function(req, res, next) {
     var sql_query = "";
 
-    console.log(req.user);
-    console.log(req.user.iscustomer);
-
-    if (req.user.iscustomer){
-        sql_query = "select * from Users natural join Customers where Users.uid = " + "'" + req.user.uid +"'";
+    if (req.user.iscustomer) {
+        sql_query =
+            "select * from Users natural join Customers where Users.uid = " +
+            "'" +
+            req.user.uid +
+            "'";
     } else {
-        sql_query = "select * from Users natural join Owners where Users.uid = " + "'" + req.user.uid +"'";
+        sql_query =
+            "select * from Users natural join Owners where Users.uid = " +
+            "'" +
+            req.user.uid +
+            "'";
     }
 
-    pool.query(sql_query,(err,data) => {
-        if (err){
+    pool.query(sql_query, (err, userData) => {
+        if (err) {
             console.log(err);
             return;
         }
-        
-        res.render('account/profile', {
 
-            title: 'User Profile',
-            currentUser: req.user,
-            data: data.rows
+        pool.query(
+            "select * from choose C natural join Incentives i where C.uid=$1",
+            [req.user.uid],
+            function(err, incentiveData) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(incentiveData);
 
-        });
-    })
+                res.render("account/profile", {
+                    title: "User Profile",
+                    currentUser: req.user,
+                    userData: userData.rows,
+                    incentiveData: incentiveData.rows
+                });
+            }
+        );
+    });
 });
 
+router.get("/reservation", checkLoggedIn, function(req, res, next) {
+    if (req.user.iscustomer) {
+        pool.query("select r.reserveid, Res.name, b.address, t.time from reserves r inner join tables t on r.reserveid=t.reserveid and r.uid=$1 and r.timestamp >= (select current_date) inner join restaurants Res on t.rid=Res.rid inner join branches b on b.rid=Res.rid and t.bid=b.bid order by b.address", [req.user.uid], function(err, data) {
+            if(err) {
+                return;
+            }
+            console.log(data.rows);
+            
+            res.render("account/reservation", {
+                currentUser: req.user,
+                data: data.rows
+            });
+        })
+    } else {
+        pool.query("select reserves.reserveid, branches.location, users.name, tables.time, reserves.guestcount from restaurants natural join owners natural join branches natural join tables inner join reserves on tables.reserveid=reserves.reserveid inner join users on reserves.uid=users.uid where owners.uid=$1 and tables.vacant=false order by branches.location;", [req.user.uid], function(err, data) {
+            if(err) {
+                return;
+            }
+            console.log(data);
+            
+            res.render("account/reservation", {
+                title: "Upcoming reservations",
+                currentUser: req.user,
+                data: data.rows
+            });
+        })
+    }
+})
 module.exports = router;
+
 
---
+++ App/routes/reserveRouter.js
@@ -0,0 +1,176 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var util = require('util');
+/* Connect to Database */
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+const {
+    checkLoggedIn,
+    checkLoggedOut,
+} = require("./middleware/auth");
+
+/* ---- GET/POST for reserve ---- */
+router.all('/', checkLoggedIn, function(req, res, next){
+    var sql_query;
+    if (req.user.iscustomer){
+        sql_query = "select distinct time from Tables T order by time";
+        pool.query(sql_query, function(err, time){
+            if(err) {
+                return;
+            }
+            sql_query = "select distinct seats from Tables T order by seats";
+            pool.query(sql_query, function(err, seats) {
+                if (err){
+                    console.log(err);
+                    return;
+                }
+                console.log(req.body)
+                res.render('restaurant/reservation', {
+                    title: 'Reservation Selection',
+                    currentUser: req.user,
+                    req: req.body,
+                    time: time.rows,
+                    data: seats.rows
+                });
+            });
+        });    
+    } else {
+        req.flash("error", "Owners are not allowed to make reservations!");
+        res.redirect("/");
+        return false;
+    }
+});
+
+router.post('/submit', checkLoggedIn, function(req, res, next){
+    pool.connect(function(err, client, done) {
+        function abort(err) {
+            if(err) {
+                client.query('ROLLBACK', function(err) {
+                    done();
+                });
+                return true;
+            }
+            return false;
+        }
+        function redirect(user, body) {
+            client.query('ROLLBACK', function(err) {
+                sql_query = "select distinct time from Tables T order by time";
+                client.query(sql_query, function(err, time){
+                    if(err) {
+                        return;
+                    }
+                    sql_query = "select distinct seats from Tables T order by seats";
+                    client.query(sql_query, function(err, seats) {
+                        if (err){
+                            console.log(err);
+                            return;
+                        }
+                        console.log(req.body)
+                        req.flash("error", "The restaurant is fully booked at the chosen timing!");
+                        res.render('restaurant/reservation', {
+                            title: 'Reservation Selection',
+                            currentUser: user,
+                            req: body,
+                            time: time.rows,
+                            data: seats.rows
+                        });
+                    });
+                });
+            });
+
+        }
+        client.query('BEGIN', function(err, res1) {
+            if(abort(err)) {
+                return;
+            }
+            console.log(req.body)
+            client.query('select t.tid from restaurants r inner join branches b on r.rid=b.rid inner join tables t on r.rid=t.rid and b.bid=t.bid where r.name=$1 and b.address=$2 and t.time=$3 and t.seats>=$4 and t.vacant=true', [req.body.name, req.body.address, req.body.time, req.body.seats], function(err, res2) {
+                if(abort(err)) {
+                    return;
+                }
+                if(!res2.rows.length) {
+                    redirect(req.user, req.body)
+                    return;
+                }
+                client.query('select now()::timestamptz(0)', function(err, res3) {
+                    if(abort(err)) {
+                        return;
+                    }
+                    var time = res3.rows[0].now
+                    client.query('Insert into reserves(uid, timestamp, guestcount) values ($1, $2, $3)', [req.user.uid, time, req.body.seats], function(err, res4) {
+                        if(abort(err)) {
+                            return;
+                        }
+                        client.query('select reserveId from reserves r where r.timestamp=$1 and r.uid=$2', [time, req.user.uid], function(err, res5) {
+                            if(abort(err)) {
+                                return;
+                            }
+                            var reserveId = res5.rows[0].reserveid
+                            console.log(reserveId)
+                            var tid = res2.rows[0].tid;
+                            client.query('Update tables set vacant=false, reserveId=$1 where tables.tid=$2', [reserveId, tid], function(err, res6) {
+                                if(abort(err)) {
+                                    return;
+                                }
+                                client.query('COMMIT', function(err, res5) {
+                                    if(abort(err)) {
+                                        return;
+                                    }
+                                    req.flash("success", "Successfully booked!")
+                                    res.redirect("/")
+                                    done();
+                                })
+                            });
+                        })
+                    })
+                    
+                })
+            });
+        });
+    });
+});
+
+router.post("/cancel", checkLoggedIn, function(req, res,next) {
+    
+        pool.connect(function(err, client, done) {
+            function abort(err) {
+                if(err) {
+                    client.query('ROLLBACK', function(err) {
+                        done();
+                    });
+                    return true;
+                }
+                return false;
+            }
+            client.query("BEGIN", function(err, res1) {
+                if(abort(err)) {
+                    return;
+                }
+                client.query("Update tables set vacant=true, reserveid=null where reserveid=$1", [req.body.reserveid], function(err, res2) {
+                    if(abort(err)) {
+                        return;
+                    }
+                    client.query("delete from reserves where reserveid=$1", [req.body.reserveid], function(err, res3) {
+                        if(abort(err)) {
+                            return;
+                        }
+                        client.query("COMMIT", function(err, res4){
+                            if(abort(err)) {
+                                return;
+                            }
+                            req.flash("success", "Reservation has been cancelled!")
+                            res.redirect("/account/reservation")
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    
+})
+module.exports = router;
+
