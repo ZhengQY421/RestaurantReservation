10cb89a37264f7583e49b15a437f4bbea22e2366
adriannafu <afu4599@gmail.com>
2019-04-12 21:22:40 +0800

    project report done

--- App/routes/branchesRouter.js
+++ App/routes/branchesRouter.js
@@ -17,7 +17,6 @@ router.get("/", function(req, res, next) {
         if (err) {
             console.log(err);
         }
-        console.log(branchData)
         pool.query(
             "select * from response right outer join ratings on response.rtid = ratings.rtid inner join users on users.uid = ratings.uid inner join gives on gives.rtid = ratings.rtid inner join branches on branches.bid = gives.bid and branches.rid = gives.rid where gives.rid = $1",
             [branchData.rows[0].rid],
@@ -25,6 +24,7 @@ router.get("/", function(req, res, next) {
                 if (err) {
                     console.log(err);
                 }
+                console.log(ratingData)
                 res.render("restaurant/branches", {
                     title: req.query.name,
                     branchData: branchData.rows,
