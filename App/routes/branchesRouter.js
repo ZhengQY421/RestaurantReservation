2a3d4487081e8491c44b2d61f1d872e13f7fdfcb
Oliver Zheng <ZhengQy421@gmail.com>
2019-04-12 20:39:06 +0800

    yay

--- App/routes/branchesRouter.js
+++ App/routes/branchesRouter.js
@@ -7,6 +7,8 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
+const { checkLoggedIn, checkLoggedOut } = require("./middleware/auth");
+
 /* ---- GET for show all details of particular restaurant branches ---- */
 router.get("/", function(req, res, next) {
     sql_query =
@@ -17,7 +19,7 @@ router.get("/", function(req, res, next) {
         }
 
         pool.query(
-            "select * from gives G natural join ratings RT natural join response R natural join Users U natural join branches where G.rid = $1",
+            "select * from response right outer join ratings on response.rtid = ratings.rtid inner join users on users.uid = ratings.uid inner join gives on gives.rtid = ratings.rtid inner join branches on branches.bid = gives.bid and branches.rid = gives.rid where gives.rid = $1",
             [branchData.rows[0].rid],
             function(err, ratingData) {
                 if (err) {
@@ -38,7 +40,7 @@ router.get("/", function(req, res, next) {
 /* ---- GET for show all ratings of particular restaurant branches ---- */
 router.get("/ratings", function(req, res, next) {
     pool.query(
-        "select * from ratings natural join branches natural join Users natural join response natural join gives where rid=$1",
+        "select * from response right outer join ratings on response.rtid = ratings.rtid inner join users on users.uid = ratings.uid inner join gives on gives.rtid = ratings.rtid inner join branches on branches.bid = gives.bid and branches.rid = gives.rid where gives.rid = $1",
         [req.query.rid],
         function(err, data) {
             if (err) {
@@ -54,9 +56,9 @@ router.get("/ratings", function(req, res, next) {
     );
 });
 
-router.post("/addReview", function(req, res, next) {
+router.post("/addReview", checkLoggedIn, function(req, res, next) {
     pool.query(
-        "insert into ratings values ($1, $2, $3)",
+        "insert into ratings (uid, score, review) values ($1, $2, $3)",
         [req.user.uid, req.body.score, req.body.review],
         function(err, data) {
             console.log(
@@ -67,12 +69,27 @@ router.post("/addReview", function(req, res, next) {
                 console.log(err);
             }
 
-            timeStamp, uid, rtid, rid, bid;
-
             pool.query(
-                "insert into gives values" +
-                    "(select now()::timestamptz(0)," +
-                    " $1, (select R.rtid from Ratings R where R.review=$2),"
+                "insert into gives (timeStamp, uid, rtid, rid, bid) values (" +
+                    "(select now()::timestamptz(0))," +
+                    " $1, (select R.rtid from Ratings R where R.review=$2), $4, (select B.bid from Branches B where B.location = $3 and B.rid=$4))",
+                [req.user.uid, req.body.review, req.body.branch, req.query.rid],
+                function(err, data) {
+                    console.log(req.body.branch);
+
+                    if (err) {
+                        console.log(err);
+                        req.flash(
+                            "error",
+                            "Review couldn't be posted due to an unknown error!"
+                        );
+                        res.redirect("/branches?name=" + req.query.name);
+                        return;
+                    }
+
+                    req.flash("success", "Review posted!");
+                    res.redirect("/branches?name=" + req.query.name);
+                }
             );
         }
     );
