b0421f3a435703b304695f277a20981cf759713d
adriannafu <afu4599@gmail.com>
2019-04-13 10:49:08 +0800

    branch

--- App/routes/branchesRouter.js
+++ App/routes/branchesRouter.js
@@ -12,33 +12,31 @@ const { checkLoggedIn, checkLoggedOut } = require("./middleware/auth");
 /* ---- GET for show all details of particular restaurant branches ---- */
 router.get("/", function(req, res, next) {
     sql_query =
-        "select R.rid, B.bid, P.file, R.name, R.type, R.description, coalesce(B.pnumber, 'No contact number available!') as pnumber, B.address, B.location FROM Photos P natural join Restaurants R inner join Branches B on R.rid=B.rid and R.name=$1";
-    pool.query(sql_query, [req.query.name], function(err, branchData) {
-        if (err) {
-            console.log(err);
-        }
-        pool.query(
-            "select * from response right outer join ratings on response.rtid = ratings.rtid inner join users on users.uid = ratings.uid inner join gives on gives.rtid = ratings.rtid inner join branches on branches.bid = gives.bid and branches.rid = gives.rid where gives.rid = $1",
-            [branchData.rows[0].rid],
-            function(err, ratingData) {
-                if (err) {
-                    console.log(err);
-                }
-                res.render("restaurant/branches", {
-                    title: req.query.name,
-                    branchData: branchData.rows,
-                    ratingData: ratingData.rows,
-                    currentUser: req.user
-                });
+        "select R.rid, B.bid, coalesce(P.file, 'No photos available!') as file, R.name, R.type, R.description, coalesce(B.pnumber, 'No contact number available!') as pnumber, B.address, B.location FROM Photos P right outer join Restaurants R on P.rid=R.rid inner join Branches B on R.rid=B.rid and R.name=$1";    pool.query(sql_query, [req.query.name], function(err, branchData) {
+    if (err) {
+        console.log(err);
+    }
+    pool.query(
+        "select * from gives G natural join ratings RT natural join response R natural join Users U natural join branches where G.rid = $1",
+        [branchData.rows[0].rid],
+        function(err, ratingData) {
+            if (err) {
+                console.log(err);
             }
-        );
+            res.render("restaurant/branches", {
+                title: req.query.name,
+                branchData: branchData.rows,
+                ratingData: ratingData.rows,
+                currentUser: req.user
+            });
+        });
     });
 });
 
 /* ---- GET for show all ratings of particular restaurant branches ---- */
 router.get("/ratings", function(req, res, next) {
     pool.query(
-        "select * from response right outer join ratings on response.rtid = ratings.rtid inner join users on users.uid = ratings.uid inner join gives on gives.rtid = ratings.rtid inner join branches on branches.bid = gives.bid and branches.rid = gives.rid where gives.rid = $1",
+        "select * from ratings natural join branches natural join Users natural join response natural join gives where rid=$1",
         [req.query.rid],
         function(err, data) {
             if (err) {
@@ -50,8 +48,7 @@ router.get("/ratings", function(req, res, next) {
                 data: data.rows,
                 currentUser: req.user
             });
-        }
-    );
+        });
 });
 
 router.post("/addReview", checkLoggedIn, function(req, res, next) {
